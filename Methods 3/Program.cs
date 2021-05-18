24ed1a8893d9241f7a937177eff2822a3b1f2505
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:55:06 +0530

    Add files via upload

---
+++ Methods 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Method4
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            int f = 1;
+
+           
+            for (int i = 1; i <= n; i++)
+            {
+                f = f * i;
+            }
+
+            return f;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            int p = 4;
+
+            
+            Console.WriteLine("Factorial is : " + factorial(p));
+        }
+    }
+}
