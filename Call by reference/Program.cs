571509e3368a9a32d886045811b3a6849792724c
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 00:04:41 +0530

    Add files via upload

---
+++ Call by reference/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Call_by_reference
+{
+    class Program
+    {
+        class NumberManipulator
+        {
+            public void swap(ref int x, ref int y)
+            {
+                int temp;
+
+                temp = x;
+                x = y;
+                y = temp;
+            }
+            static void Main(string[] args)
+            {
+                NumberManipulator n = new NumberManipulator();
+
+
+                int a = 100;
+                int b = 200;
+
+                Console.WriteLine("Before swap, value of a : {0}", a);
+                Console.WriteLine("Before swap, value of b : {0}", b);
+
+
+                n.swap(ref a, ref b);
+
+                Console.WriteLine("After swap, value of a : {0}", a);
+                Console.WriteLine("After swap, value of b : {0}", b);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
