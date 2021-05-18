042c6ae3a581e71b1c6ad9bec951e4ec8a2dd40b
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:51:42 +0530

    Add files via upload

---
+++ Methods 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Methods_3
+{
+    class Program
+    {
+        static int sum()
+        {
+            int a = 78, b = 70, add;
+            add = a + b;
+            return add;
+        }
+        static void Main(string[] args)
+        {
+
+
+            int getresult = sum();
+                Console.WriteLine(getresult);
+        }
+    }
+}
+        
