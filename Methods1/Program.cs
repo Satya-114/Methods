af7dde3dbd1ac6fdcbee94394211486f18ccead4
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:48:35 +0530

    Add files via upload

---
+++ Methods1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods_2
+{
+    class Program
+    {
+        static void PrintSentence()
+        {
+
+            Console.WriteLine("No parameters and return type void");
+        }
+
+        
+        static void Main(string[] args)
+        {
+            PrintSentence();
+        }
+    }
+}
