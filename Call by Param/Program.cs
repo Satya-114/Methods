c8e2d3265070dbd6ba06c97e998406ae445d8307
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 00:27:11 +0530

    Add files via upload

---
+++ Call by Param/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Param1
+{
+    class Program
+    {
+        static void DisplayArray(params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 3, 4, 5, 6 };
+            DisplayArray(myArray);
+            Console.WriteLine();
+            DisplayArray(2, 3, 4, 5, 6, 7, 8, 9, 10);
+            
+
+        }
+
+    }
+}
+    
