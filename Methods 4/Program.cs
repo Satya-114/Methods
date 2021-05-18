0cbc00c423e75b8cbcf2e7f02a00b7ac1a7dd38a
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:58:34 +0530

    Add files via upload

---
+++ Methods 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace method5
+{
+    class Program
+    {
+        static void perimeter(int p)
+        {
+
+            Console.WriteLine("Perimeter of the Square is " + 4 * p);
+        }
+
+        static void Main(string[] args)
+        {
+
+            
+            int p = 5;
+            perimeter(p);
+        }
+    }
+}
+       
